8831129b54f86e6b3570941e84f5c4e6b2a26246
sumanbattu <battu.suman94@gmail.com>
2024-09-21 10:45:35 +0530

    final commitss

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 Instnace Type"
   type = string
-  default = "t3.micro"
+  default = "t2.micro"
 }
 
 # AWS EC2 Instance Key Pair
