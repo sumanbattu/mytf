8969702196f63c1fbcd0954d17a7aaf73d0563b4
sumanbattu <battu.suman94@gmail.com>
2024-09-26 23:48:27 +0530

    create ec2 on push

--- ec2instance.tf
+++ ec2instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "myec2vm" {
   instance_type = var.instance_type
   //user_data = file("${path.module}/app1-install.sh")
   //key_name = var.instance_keypair
-  vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id   ]
+  #vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id   ]
   tags = {
     "Name" = "EC2 Demo 1"
   }
