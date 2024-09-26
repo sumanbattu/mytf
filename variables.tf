97926fc96430c8b1ec19bc3c97f94ad64d594312
sumanbattu <battu.suman94@gmail.com>
2024-09-26 23:46:01 +0530

    create ec2 push

--- variables.tf
+++ variables.tf
@@ -14,9 +14,11 @@ variable "instance_type" {
 }
 
 # AWS EC2 Instance Key Pair
-variable "instance_keypair" {
+/*variable "instance_keypair" {
   description = "AWS EC2 Key Pair that need to be associated with EC2 Instance"
   type = string
   default = "terraform-key"
 }
 //this is to test.
+
+*/
